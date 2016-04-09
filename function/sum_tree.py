b4eff8815e8744fcda0cd35e181d145272557d55
kzrs55 <569726429@qq.com>
2016-04-09 12:19:20 +0800

    递归求和列表中所有元素

--- function/sum_tree.py
+++ function/sum_tree.py
@@ -1,7 +1,20 @@
-#coding=utf-8
+# coding=utf-8
 __author__ = 'zjutK'
 '''利用递归处理任意结构
 [1,2,3,[1,2,3],[1,2,[1,3],]]元素求和
 '''
 
-def sum_tree(L):
+
+def sum_tree(list_1):
+    sum_1 = 0
+    for i in list_1:
+        if not isinstance(i, list):
+            sum_1 += i
+        else:
+            sum_1 += sum_tree(i)
+    return sum_1
+
+
+if __name__ == '__main__':
+    list_1 = [1, 2, 3, [1, 2, 3], [1, 2, [1, 3], ]]
+    print(sum_tree(list_1))
