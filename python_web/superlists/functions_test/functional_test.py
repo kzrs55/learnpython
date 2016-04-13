db8455874de2440713594ff65e3eff3176915d37
kzrs55 <569726429@qq.com>
2016-04-13 15:51:19 +0800

    功能测试,现在可以输入to-do

--- python_web/superlists/functions_test/functional_test.py
+++ python_web/superlists/functions_test/functional_test.py
@@ -2,6 +2,7 @@
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 __author__ = 'zjutK'
 
@@ -9,14 +10,33 @@ __author__ = 'zjutK'
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):  # 测试之前
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(30) # 等待三秒钟使得内容出现
+        self.browser.implicitly_wait(30)  # 等待三秒钟使得内容出现
 
     def tearDown(self):  # 测试之后
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):  # 名字以test_开头的都是测试
         self.browser.get('http://localhost:8000')
-        self.assertIn('Welcome', self.browser.title)
+        # 网页的标题和头部都包含To-Do这个词
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # 应用邀请她输入一个待办事项
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_atteibute('placeholder'),
+                'Enter a to-do item'
+        )
+        # 文本框输入'Buy peacock feathers'
+        inputbox.send_keys('Buy peacock feathers')
+        # 按下更新,页面显示更新
+        inputbox.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+                any(rows.text == '1: Buy peacock feathers' for rows in rows)
+        )
         self.fail('Finish the test')
 
 
