04f449a99e1e628be115108ea24ee0abf3ca46cc
kzrs55 <569726429@qq.com>
2016-04-30 17:52:45 +0800

    多线程实例

--- crawl/Fetchurls.py
+++ crawl/Fetchurls.py
@@ -22,15 +22,14 @@ class FetchUrl(threading.Thread):
                 d=request.urlopen(req)
             except URLError as e:
                 print('URL %s failed: %s' % (url, e.reason))
-            # print(d.read())
-            self.output.write(d.read().decode('utf-8'))
+            self.output.write(str(d.read()))
             print('write done by %s' % self.name)
             print('URL %s fetched by %s' % (url, self.name))
 
 def main():
     urls1=['http://www.baidu.com','http://www.qq.com']
     urls2=['http://www.neitui.me','http://www.sina.com']
-    f=open('output.txt','w+')
+    f=open('output.txt','a')
     t1=FetchUrl(urls1,f)
     t2=FetchUrl(urls2,f)
     t1.start()
