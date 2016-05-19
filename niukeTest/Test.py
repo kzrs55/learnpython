466f6d5e9f997ddfd83991487b7139f2ef1410ec
kzrs55 <569726429@qq.com>
2016-05-19 10:10:34 +0800

    测试文件

--- niukeTest/Test.py
+++ niukeTest/Test.py
@@ -2,6 +2,8 @@
 from pip._vendor.distlib.compat import raw_input
 
 __author__ = 'zjutK'
-ss=raw_input('Input a string')
+start_url={ "params": {"p":u"杭州"},"urlTemplate": "http://www.court.gov.cn/search.html?content={p}"}
 
-print(ss)
+ss='sgfdsgsadga'
+for i in ss[0]:
+    print(i)
